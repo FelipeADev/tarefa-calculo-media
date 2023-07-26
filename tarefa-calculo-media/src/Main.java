13dae27e8d79e4953b2847f8d6791aad8c806986
Felipe Aguirre <exemplo@teste.com.br>
2023-07-26 16:14:32 -0300

    First commit

---
+++ tarefa-calculo-media/src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+    public static void main(String[] args) {
+            calculoMedia();
+        }
+
+    private static void calculoMedia() {
+        float nota1 = 7;
+        float nota2 = 8;
+        float nota3 = 6;
+        float nota4 = 8;
+        float media = (nota1 +nota2 + nota3 + nota4) / 4;
+        System.out.println("Sua média é: " + media);
+    }
+}
